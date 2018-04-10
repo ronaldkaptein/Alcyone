94dc0a2b95ee7cc981c33e7314a86b70b7469f75
Ronald Kaptein <rkaptein@gnresound.com>
2018-04-10 15:47:07 +0200

    Added function to mask email address

--- functions/phpfunctions.php
+++ functions/phpfunctions.php
@@ -304,3 +304,16 @@ function get_metadata($file,$tag){
       $i++;
    }
 }
+
+function hide_email($email,$prefix)
+{ 
+   $character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+   $key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
+   for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
+   $script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+   $script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+   $script.= 'document.getElementById("'.$id.'").innerHTML="'.$prefix.'<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+   $script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
+   $script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
+   return '<span id="'.$id.'">[javascript protected address]</span>'.$script;
+}
